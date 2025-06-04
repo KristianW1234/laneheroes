930aaec9bb9ad75ba3e3d17e92c55aa02b9704b0
Kristian <kristian.wijaya1234@gmail.com>
2025-06-04 21:14:10 +0700

    Combining WebMvcConfig.java

--- src/main/java/com/personal/laneheroes/config/WebMvcConfig.java
+++ src/main/java/com/personal/laneheroes/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.personal.laneheroes.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${image-dir}")
     private String imageDir;
 
+    @Value("${spring.mvc.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/hero/**")
@@ -18,5 +22,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/images/game/**")
                 .addResourceLocations("file:///" + imageDir + "game/");
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
